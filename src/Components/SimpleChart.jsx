d7366751bcdc3849fda5fb5b7cb654ab3d16729a
MdMoin788 <developermdmoin@gmail.com>
2022-08-11 00:13:47 +0530

    simpl Chart

--- src/Components/SimpleChart.jsx
+++ src/Components/SimpleChart.jsx
@@ -1,5 +1,3 @@
-
-import Styles from "../Styles/FlowChart.module.css"
 import {
     AreaChart,
     Area,
@@ -7,48 +5,48 @@ import {
     YAxis,
     CartesianGrid,
     Tooltip
-  } from "recharts";
+} from "recharts";
+import Styles from "../Styles/FlowChart.module.css"
+
 import { useSelector } from "react-redux";
 const SimpleChart = () => {
     const data = [
         {
-          name: "Page A",
-          pv: 2400,
-          temp :1
+            Time: "10:30 am",
+            temperature: 24,
         },
         {
-          name: "Page B",
-          pv: 1398,
-          temp :2
+            Time: "10:30 am",
+            temperature: 13,
         },
         {
-          name: "Page C",
-          pv: 9800,
-          temp :3
+            Time: "10:30 am",
+            temperature: 98,
         },
         {
-          name: "Page D",
-          pv: 3908,
-          temp :4
+            Time: "10:30 am",
+            temperature: 39,
         },
         {
-          name: "Page E",
-          pv: 4800,
-          temp :5
+            Time: "10:30 am",
+            temperature: 48,
         },
         {
-          name: "Page F",
-          pv: 3800,
-          temp :6
-        },
-        {
-          name: "Page G",
-          pv: 4300,
-          temp :7
+            Time: "10:30 am",
+            temperature: 38,
         }
-      ];
-
-      const temp = [
+        ,
+        {
+            Time: "10:30 am",
+            temperature: 43,
+        }
+        ,
+        {
+            Time: "10:30 am",
+            temperature: 43,
+        }
+    ];
+    const temp = [
         28.9,
         28.97,
         28.71,
@@ -61,67 +59,63 @@ const SimpleChart = () => {
         27.91,
         28.52,
         29.88
-      ]
-      const time = [
+    ]
+    const time = [
         , "10:30 PM", "11:30 PM", "12:30 AM", "1:30 AM", "2:30 AM", "3:30 AM", "4:30 AM", "5:30 AM", "6:30 AM", "7:30 AM"
         , "8:30 AM"
         , "9:30 AM"
-      ]
+    ]
     const daily = useSelector((store) => store.weathers.weather)
     return (
-       <>
-       
-       {
-        daily == null ?
-          <>
-            <div>
-              <AreaChart
-                width={500}
-                height={200}
-                data={data}
-                syncId="anyId"
-                margin={{
-                  top: 10,
-                  right: 30,
-                  left: 0,
-                  bottom: 0
-                }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <Tooltip />
-                <Area type="monotone" dataKey="pv" stroke="#82ca9d" fill="#82ca9d" />
-              </AreaChart>
-            </div>
-          </>
-          :
-          <>
-            <div>
-              <h3 style={{ "text-align": "center", textDecoration: "underline" }}>Bar Charts</h3>
-              <AreaChart
-                width={500}
-                height={200}
-                data={data}
-                syncId="anyId"
-                margin={{
-                  top: 10,
-                  right: 30,
-                  left: 0,
-                  bottom: 0
-                }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-
-                <Tooltip />
-                <Area type="monotone" dataKey="pv" stroke="#82ca9d" fill="#82ca9d" />
-              </AreaChart>
-            </div>
-          </>
-      }
-      
-       </>
-  
+        <>
+            {
+                daily == null ?
+                    <>
+                        <div className={Styles.flowchart}>
+                            <AreaChart
+                                width={500}
+                                height={200}
+                                data={data}
+                                syncId="anyId"
+                                margin={{
+                                    top: 10,
+                                    right: 30,
+                                    left: 0,
+                                    bottom: 0
+                                }}
+                            >
+                                {/* <CartesianGrid strokeDasharray="2 2" /> */}
+                                <XAxis dataKey="Time" />
+                                <Tooltip />
+                                <Area type="monotone" dataKey="temperature" stroke="#82ca9d" fill="#82ca9d" />
+                            </AreaChart>
+                        </div>
+                    </>
+                    :
+                    <>
+                        <div className={Styles.flowchart}>
+                            <h3 style={{ "text-align": "center", textDecoration: "underline" }}>Bar Charts</h3>
+                            <AreaChart
+                                width={500}
+                                height={200}
+                                data={data}
+                                syncId="anyId"
+                                margin={{
+                                    top: 10,
+                                    right: 30,
+                                    left: 0,
+                                    bottom: 0
+                                }}
+                            >
+                                {/* <CartesianGrid strokeDasharray="2 2" /> */}
+                                <XAxis dataKey="Time" />
+                                <Tooltip />
+                                <Area type="monotone" dataKey="temperature" stroke="#82ca9d" fill="#82ca9d" />
+                            </AreaChart>
+                        </div>
+                    </>
+            }
+        </>
     )
 }
 export default SimpleChart;
